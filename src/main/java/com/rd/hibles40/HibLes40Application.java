2def401967143f62edff9458f098de36c80305da
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-05-23 00:53:54 +0300

    Initial commit

---
+++ src/main/java/com/rd/hibles40/HibLes40Application.java
@@ -0,0 +1,13 @@
+package com.rd.hibles40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibLes40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibLes40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rd/hibles40/HibLes40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rd.hibles40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibLes40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
